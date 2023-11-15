2e278702cc8e5917fafa00c57a0b961f4c18e97a
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-15 01:21:22 +0000

    Printf Project

--- printf.c
+++ printf.c
@@ -26,6 +26,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+here:
 	while (format[i] == '\0')
 	{
 		j = 12;
@@ -36,7 +37,7 @@ int _printf(const char *format, ...)
 			{
 				len += m[j].fun();
 				i += 2;
-				break;
+				goto here;
 			}
 			j--;
 		}
